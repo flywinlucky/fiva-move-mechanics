a8919aa6d0059d6976f21c1a8a5f5b2fc0fd34b3
Traffic Wolf <flywinlucky777@gmail.com>
2026-03-14 00:56:10 +0200

    fg

---
+++ Assets/Fly Studios Games/FIVA 20/Scripts/Yandex SDK/YGInitialize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class YGInitialize : MonoBehaviour
+{
+    private void Start()
+    {
+        YG2.GameReadyAPI();
+        Debug.Log("GameReady");
+
+       YG2.SwitchLanguage("en");
+    }
+}
