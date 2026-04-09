1c670645bb12f40984dbe2dc2670fe2a316a1549
Traffic Wolf <flywinlucky777@gmail.com>
2026-04-09 11:55:01 +0300

    erf

---
+++ Assets/Fly Studios Games/FIVA 20/Scripts/RoundTeamsResuldInWord.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class RoundTeamsResuldInWord : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public TMP_Text blue_Name;
+    public TMP_Text blue_Score;
+    [Space]
+    public TMP_Text red_Name;
+    public TMP_Text red_Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
